7111626e7f99a104421c2799626a6b82a4073a10
AYOMIDE JONES <AYORMEDAY@GMAIL.COM>
2026-02-21 22:43:10 +0100

    cleanup

--- src/helper.ts
+++ src/helper.ts
@@ -1,11 +1,5 @@
 import { Product } from "./product";
 
-// Round to 2 d.p function
-// const roundValue = (value: number): number => {
-//   return Math.round(value * 100) / 100;
-//   return Number(value.toFixed(2)); 
-// };
-
 //Check Duplicates by Product name
 const checkDuplicate = (productA: Product, productB: Product): boolean => {
   return productA.name === productB.name && productA.price === productB.price;
